0dc46eb9362e72f0ed613b4712e88416fd72d0cf
Rup Narayan Yadav <rupnarayan.23ise@cambridge.edu.in>
2026-08-08 22:29:09 +0530

    Time: 1 ms (97.52%) | Memory: 47.3 MB (78.77%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,15 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        
+        int totalSum=0;
         for(int i=0;i<nums.length;i++){
-            int leftsum=0;
-            for(int j=0;j<i;j++){
-                leftsum+=nums[j];
-            }
-            int rightsum=0;
-            for(int k=i+1;k<nums.length;k++){
-                rightsum+=nums[k];
-            }
-            if(leftsum==rightsum){
+            totalSum+=nums[i];
+        }
+        int leftSum=0;
+        for(int i=0;i<nums.length;i++){
+            if(leftSum==totalSum-leftSum-nums[i]){
                 return i;
             }
+            leftSum+=nums[i];
         }
         return -1;
     }
