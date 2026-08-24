833e43c73fe9cfed583abfb417f38ab31fc1e6fd
Rup Narayan Yadav <rupnarayan.23ise@cambridge.edu.in>
2026-08-24 19:07:47 +0530

    Time: 465 ms (67.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT salary
+      FROM(
+        SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+        FROM Employee
+      ) AS ranked
+      WHERE rnk=N
+  );
+END
